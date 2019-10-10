e75c82cf3533d01b6c2bc19c43735ed19fa26a8f
jeriveramoya@suse.com <Joaqun Rivera>
2019-10-10 13:59:26 +0200

    Add aruba support

--- lib/libyui_client/widget_controller.rb
+++ lib/libyui_client/widget_controller.rb
@@ -2,19 +2,6 @@
 
 # Client to interact with YAST UI rest api framework for integration testing
 module LibyuiClient
-#  def self.find_widgets(id: nil, label: nil, type: nil)
-#    params = {}
-#    params[:id] = id if id
-#    params[:label] = label if label
-#    params[:type] = type if type
-#
-#    send_request(:get, '/widgets', params)
-#  end
-#
-#  def self.find_widget(type: nil, label: nil, id: nil)
-#    find_widgets(type: type, label: label, id: id).first
-#  end
-
   def self.change_widget(action: nil, id: nil, type: nil, label: nil,
                         value: nil, column: nil)
     params = {}
