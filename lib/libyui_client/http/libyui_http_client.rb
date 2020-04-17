fd4e1627b94efa37ee793c8790ea3f82dfcc6f1a
Oleksandr Orlov <oleksandr.orlov1@gmail.com>
2020-04-17 02:03:06 +0200

    Implement Http client infrastructure

---
+++ lib/libyui_client/http/libyui_http_client.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module LibyuiClient
+  module Http
+    class LibyuiHttpClient
+      def initialize(app)
+        @app = app
+      end
+
+      def widget_get(filter, timeout: LibyuiClient.timeout, interval: LibyuiClient.interval)
+        res = nil
+        Wait.until(timeout: timeout, interval: interval) do
+          uri = HttpClient.compose_uri(@app.host, @app.port, '/widgets', filter)
+          res = HttpClient.http_get(uri)
+          Response.new(res) if res.is_a?(Net::HTTPOK)
+        end
+      rescue Error::TimeoutError
+        rescue_widget_errors(res)
+      end
+
+      def widget_send_action(filter, action, timeout: LibyuiClient.timeout, interval: LibyuiClient.interval)
+        res = nil
+        Wait.until(timeout: timeout, interval: interval) do
+          uri = HttpClient.compose_uri(@app.host, @app.port, '/widgets',
+                                       filter.merge(action))
+          res = HttpClient.http_post(uri)
+          Response.new(res) if res.code.to_i == 200
+        end
+      rescue Error::TimeoutError
+        rescue_widget_errors(res)
+      end
+
+      private
+
+      def rescue_widget_errors(response)
+        raise Error::WidgetNotFoundError if response.is_a?(Net::HTTPNotFound)
+
+        raise Error::ItemNotFoundInWidgetError if response.is_a?(Net::HTTPUnprocessableEntity)
+
+        raise Error::LibyuiClientError
+      end
+    end
+  end
+end
