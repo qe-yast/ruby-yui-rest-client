3a38d0f44eadec85f164cebbf1fc498673209dfc
Oleksandr Orlov <oleksandr.orlov1@gmail.com>
2020-04-17 02:04:22 +0200

    Add waiting functions

--- lib/libyui_client/timer.rb
+++ lib/libyui_client/timer.rb
@@ -1,2 +1,17 @@
-module Timer
+# frozen_string_literal: true
+
+module LibyuiClient
+  module Timer
+    module_function
+
+    def wait(timeout, interval)
+      return yield if timeout.zero?
+
+      end_time = Time.now.to_f + timeout
+      while Time.now.to_i <= end_time
+        yield
+        sleep interval
+      end
+    end
+  end
 end
--- lib/libyui_client/waitable.rb
+++ lib/libyui_client/waitable.rb
@@ -1,4 +1,15 @@
+# frozen_string_literal: true
+
 module LibyuiClient
   module Waitable
+    def wait_until(timeout: LibyuiClient.timeout, interval: LibyuiClient.interval, message: nil, &block)
+      Wait.until(timeout: timeout, interval: interval, message: message, object: self, &block)
+      self
+    end
+
+    def wait_while(timeout: LibyuiClient.timeout, interval: LibyuiClient.interval, message: nil, &block)
+      Wait.while(timeout: timeout, interval: interval, message: message, object: self, &block)
+      self
+    end
   end
 end
