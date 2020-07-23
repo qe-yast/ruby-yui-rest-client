c4460117925fe71821d8ac4f70e5d17f2c8b8377
Joaquín Rivera <jeriveramoya@suse.com>
2020-07-23 15:53:18 +0200

    blablab

--- lib/libyui_client/sample.rb
+++ lib/libyui_client/sample.rb
@@ -1,6 +1,6 @@
 require 'libyui_client'
 
 # app = LibyuiClient::App.new(host: ENV['GUEST_IP'] , port: '9999')
-app = LibyuiClient::App.new(host: '127.0.0.1' , port: '9998')
+app = LibyuiClient::App.new(host: 'localhost' , port: '9998')
 button = app.button(id: 'add')
 button.click
