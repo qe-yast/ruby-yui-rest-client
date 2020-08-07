260a5f954774abae8663a66681b99c94ae3d2c48
Rodion Iafarov <riafarov@suse.com>
2020-08-07 14:26:08 +0200

    Add tests for TimeField widget

---
+++ spec/unit/widgets/timefield_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'rspec'
+
+module LibyuiClient
+  module Widgets
+    RSpec.describe Timefield do
+      let(:id) { { id: 'time' } }
+      let(:widget_controller) { instance_double('widget_controller') }
+      let(:filter) { FilterExtractor.new(id) }
+      let(:set_params) { { action: 'enter_text', value: '17:29:59' } }
+      subject { Timefield.new(widget_controller, filter) }
+
+      describe '#set' do
+        context 'use DateTime to set value' do
+          it 'it sets date field in ISO 8601 format' do
+            expect(widget_controller).to receive(:send_action).with(id, set_params)
+            expect(subject.set(DateTime.new(2001, 2, 3, 17, 29, 59))).to equal(subject)
+          end
+        end
+        context 'use Time to set value' do
+          it 'it sets date field in ISO 8601 format' do
+            expect(widget_controller).to receive(:send_action).with(id, set_params)
+            expect(subject.set(Time.new(2001, 2, 3, 17, 29, 59))).to equal(subject)
+          end
+        end
+        context 'use Date to set time field value' do
+          it 'it raises LibyuiClientError' do
+            expect { subject.set(Date.new(1998, 1, 1)) }.to raise_error(Error::LibyuiClientError)
+          end
+        end
+      end
+    end
+  end
+end
